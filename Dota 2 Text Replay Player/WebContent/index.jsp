ba9f6eb2d45ca74fc26636b44ee16b2aba1d70ab
Gabriel Cardona <gcardonagarcia@gmail.com>
2013-07-05 00:23:38 -0400

    Modified index.jsp to display level up instances. Added TestServlet to connect the page to the replay parser output. Added jQuery library. TODO: Fix Div within table issue with relation to jQuery selector for toggleSlide.

--- Dota 2 Text Replay Player/WebContent/index.jsp
+++ Dota 2 Text Replay Player/WebContent/index.jsp
@@ -1,12 +1,64 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page import="com.dota2textplay.test.*" %>
+<%@ page import="java.util.ArrayList" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
 <title>Insert title here</title>
+
+<style>
+.timeslot {
+	background-color: #a8a8a8;
+	width: auto;
+	display: inline-block;
+	margin: 10px;
+	border-radius: 5px;
+	padding: 5px;
+}
+
+.mid {
+	background-color: black;
+}
+</style>
 </head>
 <body>
-
+<script src="WEB-INF/lib/jquery-1.10.2.min.js"></script>
+<a href="TestServlet">CLICK HERE FOR TEST</a>
+<br>
+<table style="border-collapse: collapse;">
+<c:set var="flag" value="true" />
+<c:forEach var="level" items="${levelUps}">
+	<tr>
+		<c:if test="${flag eq true}">
+			<td class="left"></td>
+			<td class="mid"></td>
+		</c:if>
+		<td>
+			<div class="timeslot">
+				<div class="top">
+				${level.getTime()}<br>
+				</div>
+				<div class="bot">
+				${level.getHero()} reached level ${level.getLevel()}!<br>
+				Replay Time: ${level.getReplaytime()}<br>
+				</div>
+			</div>
+		</td>
+		<c:if test="${flag eq false}">
+			<td class="mid"></td>
+			<td class="right"></td>
+		</c:if>
+		<c:set var="flag" value="${!flag}" />
+	</tr>
+</c:forEach>
+</table>
+<script> 
+  $(".top").click(function(){
+    $(this).siblings(".bot").slideToggle("fast");
+  });</script>
 </body>
 </html>
--- Dota 2 Text Replay Player/src/com/dota2textplay/test/TestServlet.java
+++ Dota 2 Text Replay Player/src/com/dota2textplay/test/TestServlet.java
@@ -1,11 +1,23 @@
 package com.dota2textplay.test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * Servlet implementation class TestServlet
@@ -22,10 +34,63 @@ public class TestServlet extends HttpServlet {
     }
 
 	/**
+	 * USE GET FOR NOW
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//request.getParameter("replay");
+		
+		String replay_num = "223576599";
+		// TODO Auto-generated method stub
+		/*** RUNNING PARSER ON SAMPLE REPLAY ***/
+		/*String cmd[] = {"C:\\Users\\Gabriel\\Desktop\\Dota2_ReplayParser\\DotaParser.exe", "C:\\Users\\Gabriel\\Desktop\\Dota2_ReplayParser\\Replays\\" + replay_num + ".dem"};
+
+		try{
+			Process p = Runtime.getRuntime().exec(cmd);
+			//p.waitFor();
+		}
+		catch(Throwable t){
+			System.out.println("Execution Failed!");
+		}*/
+		
+		/*** PARSING PARSER OUTPUT ***/
+		String path = "C:\\Users\\Gabriel\\Desktop\\Dota2_ReplayParser\\json\\" + replay_num + "\\levelups.json";
+		//System.out.println(path);
+		File file = new File(path);
+		String text = "";
+		try{
+			text = FileUtils.readFileToString(file);
+		}
+		catch(Exception e){
+			System.out.println("JSON read failed.");
+		}
+		
+		Gson gson = new Gson();
+		JsonParser parser = new JsonParser();
+		JsonObject JsonObj = parser.parse(text).getAsJsonObject();
+		
+		JsonArray Jarray = JsonObj.getAsJsonArray("leveluptimes");
+		
+		ArrayList<LevelUp> levelUps = new ArrayList<LevelUp>();
+		
+		for(JsonElement obj : Jarray )
+	    {
+	        LevelUp cse = gson.fromJson( obj , LevelUp.class);
+	        levelUps.add(cse);
+	    }
+		
+		/*for(LevelUp lvl : levelUps)
+		{
+			System.out.println(lvl.showInfo());
+		}*/
+		request.setAttribute("levelUps", levelUps);
+		try {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+            rd.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 
 	/**
